3f095defb74ade7524151a96e836057582220f3d
Tero Väisänen <93595541+tkvaisanen@users.noreply.github.com>
2022-10-12 01:23:40 +0300

    added name form

--- hello.py
+++ hello.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "My super secret key that no one is supposed to know"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your Name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Create a route decorator
 @app.route('/')
@@ -47,3 +56,17 @@ def page_not_found(e):
 
 def page_not_found(e):
     return render_template('500.html'), 500
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        
+    return render_template('name.html',
+        name = name,
+        form = form)
