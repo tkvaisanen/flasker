80c299fdc0614fde4addaea3647e458788ddae31
Tero Väisänen <93595541+tkvaisanen@users.noreply.github.com>
2022-10-12 12:40:15 +0300

    some css and javascript

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -32,7 +32,7 @@ class NamerForm(FlaskForm):
 def index():
     first_name = "Tero"
     stuff = "This is <strong>Bold</strong> Text"
-
+    
     favorite_pizza = ["pepperoni", "Onion", "Cheese", "Bell Pepper", 41]
     return render_template('index.html', first_name=first_name, stuff=stuff,
     favorite_pizza=favorite_pizza)
@@ -66,6 +66,7 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
+        flash("Form Submitted Successfully!")
         
     return render_template('name.html',
         name = name,
